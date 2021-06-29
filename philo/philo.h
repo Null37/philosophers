2756c0c39efdbf27ec6681d4a45235c26cc2dfa9
Saad allah Samadi <ssamadi@e0r2p1.1337.ma>
2021-06-29 12:50:49 +0100

    philo

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: ssamadi <ssamadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 15:29:53 by ssamadi           #+#    #+#             */
-/*   Updated: 2021/06/27 12:31:57 by ssamadi          ###   ########.fr       */
+/*   Updated: 2021/06/29 12:50:27 by ssamadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ typedef struct s_all
 	int time_to_sleep;
 	int time_to_die;
 	int number_philo;
+	int	number_philo_must_eat;
 	pthread_mutex_t *forks;
 	pthread_mutex_t write_lock;
 }   t_all;
